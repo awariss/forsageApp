aecb4774b5c2db3306933d25f00d143115c4b416
Jan Osolsobe <xosols00@fit.vutbr.cz>
2021-03-18 08:14:57 +0100

    add webview

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,8 +1,12 @@
 <template>
     <Page>
-        <ActionBar title="Forsage App"/>
+        <ActionBar title="Forsage App" />
+
         <GridLayout columns="*" rows="*">
-        <Button text="Here man!" @tap="onButtonTap"> </Button>
+        <WebView src="https://app.forsage.net/" />
+
+        <Button text="what is your token " @tap="doGetCurrentPushToken"> </Button>
+
         </GridLayout>
     </Page>
 </template>
@@ -10,42 +14,45 @@
 <script >
 import { Dialogs } from "@nativescript/core";
 
+var firebase = require("@nativescript/firebase").firebase;
 
 export default {
     methods: {
-    
+
+    showSideDrawer() {
+      console.log("Show SideDrawer tapped.");
+      // Show sidedrawer ...
+  },
+
+
     doGetCurrentPushToken() {
     firebase.getCurrentPushToken().then(function (token) {
       // may be null if not known yet
       console.log("Current push token: " + token);
+      Dialogs.alert({
+        title: "Your token",
+        message: token,
+        okButtonText: "Close"
+      }).then(function () {
+        console.log("Dialog closed!");
+      });
+
       dialogs.alert({
         title: "Current Push Token",
         message: (token === null ? "Not received yet" : token),
         okButtonText: "OK, thx"
       });
     });
-    
-    
-      onButtonTap() {
-        console.log("Button was pressed");
-        Dialogs.alert({
-          title: "Your token",
-          message: this.token,
-          okButtonText: "Close"
-        }).then(function () {
-          console.log("Dialog closed!");
-        });
-
-    },
   },
+},
 
     data() {
       return {
         msg: 'Hello World!',
-        token: 'token number'
     }
   }
 }
+
 </script>
 
 <style scoped>
@@ -58,9 +65,10 @@ export default {
       border: none;
       color: #ffffff;
       padding: 15px 32px;
-      text-align: center;
       text-decoration: none;
       display: inline-block;
       font-size: 16px;
+      position:relative;
+      margin-top:90%;
     }
 </style>
